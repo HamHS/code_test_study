d75331a478273c0835ac4305e337c78a71b9c455
HamHS <ioio154@naver.com>
2025-06-13 13:12:56 +0900

    search

--- test.py
+++ test.py
@@ -1,20 +1,11 @@
-import sys
-
-datas = sys.stdin.read()
-datas = datas.strip().split("\n")
-datas = datas[1:]
-
-vowels = ['A','E','I','O','U','a','e','i','o','u']
-alphabet_upper = [chr(i) for i in range(65,91)]
-alphabet_lower = [chr(i) for i in range(97,123)]
-alphabet = alphabet_lower + alphabet_upper
-
-for sentence in datas:
-    consonant = 0
-    vowel = 0
-    for letter in sentence:
-        if letter in vowels:
-            vowel += 1
-        elif letter in alphabet:
-            consonant += 1
-    print(consonant, vowel)
+def binary_search(array,start,end,target):
+    if start > end:
+        return False
+    mid = (end+start)//2
+    if array[mid] == target:
+        return mid
+    elif target > array[mid]:
+        binary_search(array,mid+1,end,target)
+    elif target < array[mid]:
+        binary_search(array,start,mid-1,target)
+    
